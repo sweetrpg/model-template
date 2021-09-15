0b31044b57fb8c2a123ead88e4ae3ff3066e4f8c
Paul Schifferer <paul@schifferers.net>
2021-09-14 21:26:25 -0700

    Move some attributes.

--- src/BASE_PACKAGE/__init__.py
+++ src/BASE_PACKAGE/__init__.py
@@ -2,3 +2,14 @@
 __author__ = "Paul Schifferer <paul@schifferers.net>"
 """
 """
+
+__title__ = 'PACKAGE_NAME'
+__description__ = 'PACKAGE_DESC'
+__url__ = 'PACKAGE_URL'
+__version__ = '0.0.0'
+__build__ = 0x000000
+__author__ = 'PACKAGE_AUTHOR'
+__author_email__ = 'PACKAGE_AUTHOR_EMAIL'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 SweetRPG'
+__cake__ = u'\u2728 \U0001f370 \u2728'
